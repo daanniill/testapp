f868268fe5254535f891ed8dee61ccaaea745ed1
daanniill <daniil.malakhov12@gmail.com>
2025-03-29 16:46:31 -0700

    sample project complete

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(executable_path="chromedriver.exe")
@@ -8,9 +11,21 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-input_element.send_keys("kittens")
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
 
-time.sleep(10)
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
+input_element.send_keys("kittens" + Keys.ENTER)
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "kittens"))
+)
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "kittens")
+link.click()
+
+time.sleep(15)
 
 driver.quit()
